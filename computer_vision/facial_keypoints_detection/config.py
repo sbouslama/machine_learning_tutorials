7d347dfaacda7459ca8ddd21060beb34c837af7e
bouslama <sirine_bouslama@hotmail.fr>
2021-11-08 15:43:57 +0100

    add facial keypoints notebook

---
+++ computer_vision/facial_keypoints_detection/config.py
@@ -0,0 +1,18 @@
+TRAIN_IMG_DIR = 'data/training/'
+TEST_IMG_DIR = 'data/test/'
+
+TRAIN_CSV = 'data/training_frames_keypoints.csv'
+TEST_CSV = 'data/test_frames_keypoints.csv'
+
+DEVICE = 'cuda'
+
+BATCH_SIZE = 16
+H_IMG_SIZE = 140
+W_IMG_SIZE = 140
+
+LR = 0.001
+EPOCHS = 10
+MODEL_NAME = 'resnet18' 
+OUTPUT_SIZE = 136 
+
+MODEL_PATH = "./models"
---
+++ computer_vision/facial_keypoints_detection/data/readme.md
@@ -0,0 +1,2 @@
+# How to download the data:
+The data can be downloader via this [link](https://www.kaggle.com/julianlenkiewicz/facialkeypoints68dataset?select=test)
